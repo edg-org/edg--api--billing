3c8eab9181729ed288508b3e7ca61bc6bd27b54e
Dian DIALLO <diandiallo33@gmail.com>
2023-06-17 21:44:58 +0200

    Refactoring invoices pricing

--- api/services/PricingService.py
+++ api/services/PricingService.py
@@ -1,28 +1,12 @@
 from api.schemas.PricingSchema import Slice, PostpaidPricing, PrepaidPricing
+from pydantic import parse_obj_as
+from api.services.UtilsService import UtilsService
 
 class PricingService:
     def __init__(self):
-            self.postpaid_pricing = PostpaidPricing(
-                domestic = [
-                        Slice(name = "Slice 1", lower_index = 1.00, upper_index = 40.00, unit_price = 90.00),
-                        Slice(name = "Slice 2", lower_index = 41.00, upper_index = 330.00, unit_price = 293.00),
-                        Slice(name = "Slice 3", lower_index = 331.00, upper_index = 1000000000.00, unit_price = 336.00)
-                ],
-               private_level1 = [],
-               private_level2 = [],
-               institution = [],
-               administration = []
-            ) # TODO: these data must be to retrieve from referential end-point or from this microservice local table
+            self.postpaid_pricing = parse_obj_as(PostpaidPricing, UtilsService.get_json_data("PostpaidPricing.json"))
+            self.prepaid_pricing = parse_obj_as(PrepaidPricing, UtilsService.get_json_data("PrepaidPricing.json"))
 
-            self.prepaid_pricing = PrepaidPricing(
-                domestic_level1 = [
-                        Slice(name = "Slice 1", lower_index = 1.00, upper_index = 40.00, unit_price = 255.00),
-                        Slice(name = "Slice 2", lower_index = 41.00, upper_index = 1000000000.00, unit_price = 347.00)
-                ],
-               domestic_level2 = [Slice(name = "Slice unique", lower_index = 1.00, upper_index = 1000000000.00, unit_price = 359.00)],
-               institution_level1 = [],
-               institution_level2 = []
-            ) # TODO: these data must be to retrieve from referential end-point or from this microservice local table
 
     def get_postpaid_unit_price(self, index_value) -> float:
         slice = [slice for slice in self.postpaid_pricing.domestic if slice.lower_index <= index_value <= slice.upper_index][-1]
@@ -31,3 +15,4 @@ class PricingService:
     def get_prepaid_unit_price(self, power_subscriber) -> float:
         slice = [slice for slice in self.prepaid_pricing.domestic_level1 if slice.lower_index <= power_subscriber <= slice.upper_index][-1]
         return slice.unit_price
+    
--- api/services/UtilsService.py
+++ api/services/UtilsService.py
@@ -1,4 +1,6 @@
 import uuid
+import json
+from api.services.ExceptionService import ExceptionService
 
 class UtilsService:
     POSTPAID = "postpaid"
@@ -9,6 +11,21 @@ class UtilsService:
     def __init__(self):
         pass
 
+
     @classmethod
     def generate_uuid(cls) -> str:
         return str(uuid.uuid4())
+
+
+    @classmethod
+    def get_json_data(cls, file_name: str):
+        try:
+            with open('../data/' + file_name, 'r') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            print("File not found!")
+            ExceptionService.internal_server_error()
+        except json.JSONDecodeError:
+            print("Invalid JSON format!")
+            ExceptionService.internal_server_error()
+            
